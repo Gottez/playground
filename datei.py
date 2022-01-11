d21e47d4f03c642e20433e70b3c32e6adc76b0bc
Marco Leimer <sgt.basher@gmail.com>
2022-01-11 19:01:50 +0100

    erster code

--- datei.py
+++ datei.py
@@ -1,1 +1,16 @@
-print("Das ist ein Test")
+while True:
+    try:
+        a = int(input("Enter a number"))
+        b = int(input("Enter a second number"))
+        if (a % 6) == 0:
+            print("One number is divisible by 6")
+        elif (b % 6) == 0:
+            print("One number is divisible by 6")
+        else:
+            print("None of the numbers is divisible by 6")
+
+        if (a % 10) == 0 and (b % 10) == 0:
+            print("Both numbers are divisible by 10")
+        break
+    except:
+        print("Please enter a number")
