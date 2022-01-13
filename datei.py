7f3e4c45bf29cca34dbb0bb383fe990a436902da
Marco Leimer <sgt.basher@gmail.com>
2022-01-13 10:59:16 +0100

    hallo

--- datei.py
+++ datei.py
@@ -4,7 +4,7 @@ while True:
         b = int(input("Enter a second number"))
         if (a % 6) == 0:
             print("One number is divisible by 6")
-        elif (b % 6) == 0:
+        if (b % 6) == 0:
             print("One number is divisible by 6")
         else:
             print("None of the numbers is divisible by 6")
@@ -14,5 +14,3 @@ while True:
         break
     except:
         print("Please enter a number")
-# das ist ein test 1.2
-im kreis herum
